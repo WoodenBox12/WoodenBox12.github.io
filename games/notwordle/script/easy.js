72f5408c0fd9216fd80c49f089d7f86622b201f5
WoodenBox12 <ollysmith557@gmail.com>
2022-04-13 12:51:53 +0100

    added backspace

--- games/notwordle/script/easy.js
+++ games/notwordle/script/easy.js
@@ -7,40 +7,38 @@ function ifEasy() {
 
         alert("easy mode selected")
 
-        
+        document.addEventListener("keydown", function (event) {
+
+            if (finishedRow == true) {
+
+            }
+
+            else if (event.code === "Backspace" || event.code === "Delete") {
+                document.getElementById(`box${--currentBox}`).innerHTML = "";
+            }
+
+            else if (event.code == "Enter") { // enter
+
+                for (let i = 1; i <= 5; i++) {
+
+                    document.getElementById(`box${i}`).style.backgroundColor = "green"
+                }
+            }
+
+            else if (currentBox != 6) {
+
+                currentLetter = (event.code).charAt(3);
+
+                document.getElementById(`box${currentBox}`).innerHTML = currentLetter;
+                currentBox += 1;
+
+            }
+        });
     }
 }
 
 var currentBox = 1;
 var currentLetter;
-var finished = false;
-
-document.addEventListener("keypress", function (event) {
-
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-    });
-
-    if (params.mode == "easy") {
-
-        if (finished == true) {
-
-        }
-
-        else if (event.code == "Enter") { // enter
-
-            for (let i = 1; i <= 5; i++) {
-                document.getElementById(`box${i}`).style.backgroundColor = "green"
-            }
-        }
-
-        else if (currentBox <= 5) {
-            currentLetter = (event.code).charAt(3);
-
-            document.getElementById(`box${currentBox}`).innerHTML = currentLetter;
-            currentBox += 1;
-        }
-    }
-});
+var finishedRow = false;
 
 window.onload = ifEasy();
